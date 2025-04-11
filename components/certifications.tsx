3a0b630da54adc283936c27c2d50e56eda8fbb71
Emil <emilshamen@gmail.com>
2025-04-11 22:01:29 +0530

    added AWS SAA badge

--- components/certifications.tsx
+++ components/certifications.tsx
@@ -19,6 +19,32 @@ export default function Experience() {
       <SectionHeading>Certifications & Achievements</SectionHeading>
       <div className="flex flex-wrap justify-center max-w-[45rem]">
         <a
+          href="https://www.credly.com/badges/1aa0fe83-222d-41fd-9c16-bb70094bbcdc/public_url"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <motion.div
+            className={
+              "max-w-[20rem] p-5 m-2 rounded-xl shadow-lg bg-gradient-to-r from-gray-800 to-sky-900/10 overflow-hidden cursor-pointer"
+            }
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <div className="text-center">
+              <img
+                src="/aws-saa.png"
+                alt="Certification Logo"
+                className="w-[13rem] mx-auto mb-[-1rem]"
+              />
+              <h3 className="font-bold text-lg mt-8">
+                AWS Certified Solutions Architect – Associate
+              </h3>
+              <p className="font-normal text-gray-500">April 2025</p>
+            </div>
+          </motion.div>
+        </a>
+
+        <a
           href="https://www.credly.com/badges/06436ad7-08e2-4dc6-9328-523645e693f4/public_url"
           target="_blank"
           rel="noopener noreferrer"
@@ -44,7 +70,7 @@ export default function Experience() {
           </motion.div>
         </a>
 
-        <a
+        {/* <a
           href="https://www.credly.com/badges/527de692-7a52-47a9-9ac4-bc9e9df5aeb4/public_url"
           target="_blank"
           rel="noopener noreferrer"
@@ -66,6 +92,34 @@ export default function Experience() {
                 Google Cybersecurity Professional Certificate
               </h3>
               <p className="font-normal text-gray-500">Feb 2024</p>
+            </div>
+          </motion.div>
+        </a> */}
+
+        <a
+          href="https://coursera.org/verify/7753LGQFJT5T"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <motion.div
+            className={
+              "max-w-[20rem] w-full sm:max-w-[40rem] p-3 m-2 rounded-xl shadow-lg bg-gradient-to-r from-gray-800 to-sky-900/10 overflow-hidden cursor-pointer"
+            }
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <div className="flex items-center">
+              <img
+                src="/googlecsc.png"
+                alt="Meta Logo"
+                className="w-[3.5rem] mr-4"
+              />
+              <div className="text-left">
+                <h3 className="font-bold text-lg">
+                  Google Cybersecurity Professional Certificate
+                </h3>
+                <p className="font-normal text-gray-500">Feb 2024</p>
+              </div>
             </div>
           </motion.div>
         </a>
